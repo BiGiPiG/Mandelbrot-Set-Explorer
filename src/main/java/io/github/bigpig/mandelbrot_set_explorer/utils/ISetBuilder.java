b12e6f8d0f89456cc43671468170dae646ce5f93
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-12-28 10:59:16 +0300

    refactor: delete unused method

--- src/main/java/io/github/bigpig/mandelbrot_set_explorer/utils/ISetBuilder.java
+++ src/main/java/io/github/bigpig/mandelbrot_set_explorer/utils/ISetBuilder.java
@@ -3,15 +3,5 @@ package io.github.bigpig.mandelbrot_set_explorer.utils;
 import javafx.scene.image.WritableImage;
 
 public interface ISetBuilder {
-
-    ComplexNumber bottomLeftPoint = new ComplexNumber(-2.0, -1.5);
-    ComplexNumber topRightPoint = new ComplexNumber(1.0, 1.5);
-
     void build(WritableImage image, ComplexNumber bottomLeft, ComplexNumber topRight);
-
-    static ComplexNumber computeComplexNumber(Point point, int width, int height) {
-        double cx = bottomLeftPoint.getX() + (topRightPoint.getX() - bottomLeftPoint.getX()) * point.getX() / width;
-        double cy = topRightPoint.getY() - (topRightPoint.getY() - bottomLeftPoint.getY()) * point.getY() / height;
-        return new ComplexNumber(cx, cy);
-    }
 }
